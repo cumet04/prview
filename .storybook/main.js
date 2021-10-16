be3f97c71f38b196deb550d15f241b0259935229
cumet04 <medalhkr@gmail.com>
2021-10-16 12:21:44 +0900

    clean & fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,22 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
+    "@storybook/addon-svelte-csf",
   ],
-  "core": {
-    "builder": "storybook-builder-vite"
+  core: {
+    builder: "storybook-builder-vite",
   },
-  "svelteOptions": {
-    "preprocess": require("../svelte.config.js").preprocess
-  }
-}
+  async viteFinal(config, { configType}){
+    // refs https://issueexplorer.com/issue/eirslett/storybook-builder-vite/90
+    config.server.force = false;
+    return config
+  },
+  svelteOptions: {
+    preprocess: import("../svelte.config.mjs").preprocess,
+  },
+};
