6825a7e62df22d8d313b48aa208c3145f58f0ed9
cumet04 <medalhkr@gmail.com>
2021-10-16 09:57:34 +0900

    npx sb init --builder storybook-builder-vite

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf"
+  ],
+  "core": {
+    "builder": "storybook-builder-vite"
+  },
+  "svelteOptions": {
+    "preprocess": require("../svelte.config.js").preprocess
+  }
+}
