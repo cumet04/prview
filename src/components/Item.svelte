a433b853271cd906acb18b530a6ab2383cbf29a5
cumet04 <medalhkr@gmail.com>
2021-12-19 17:37:05 +0900

    item main

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -6,13 +6,85 @@
   export let status: "open" | "closed" | "merged";
 </script>
 
-<div>
-  <p>{status}</p>
-  <img src={iconUrl} alt="" />
-  <p>{number}</p>
-  <p>{title}</p>
-  <p>{branch}</p>
+<div class={`item ${status}`}>
+  <div class="thumbnail">
+    <img src={iconUrl} alt="" />
+  </div>
+
+  <div class="content">
+    <div class="info">
+      <div class="title">
+        <div class="number">#{number}</div>
+        <div class="name">{title}</div>
+      </div>
+      <div class="branch">
+        <div class="name">{branch}</div>
+        <button class="copy">(todo)</button>
+      </div>
+    </div>
+
+    <div class="sub" />
+  </div>
 </div>
 
-<style>
+<style lang="postcss">
+  .item {
+    display: flex;
+    width: 100%;
+    height: 64px;
+    border-left: solid 4px;
+
+    &.open {
+      border-left-color: #1a7f37;
+    }
+    &.closed {
+      border-left-color: #cf222e;
+    }
+    &.merged {
+      border-left-color: #8250df;
+    }
+  }
+
+  .thumbnail {
+    padding: 10px 8px 12px;
+    height: 100%;
+
+    & > img {
+      height: 42px;
+    }
+  }
+
+  .content {
+    flex-grow: 1;
+    display: flex;
+  }
+
+  .info {
+    flex-grow: 1;
+    height: 100%;
+    padding-top: 10px;
+    border-bottom: solid 1px #e5e5e5;
+
+    & > .title > * {
+      display: inline;
+      font-size: 18px;
+      line-height: 1;
+    }
+    & > .title > .number {
+      color: #888282;
+    }
+
+    & > .branch {
+      line-height: 1;
+      margin-top: 2px;
+      & > .name {
+        display: inline;
+        color: #888282;
+        font-size: 14px;
+      }
+    }
+  }
+
+  .sub {
+  }
 </style>
