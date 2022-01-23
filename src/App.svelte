8f194d3d5ef1633637d298fe59940bd100a615d1
cumet04 <medalhkr@gmail.com>
2022-01-23 22:15:28 +0900

    app dem

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
+  import Item from "@/components/Item.svelte";
 </script>
 
-<main>
-  <p>hoge</p>
+<main class="_color-theme-root-light">
+  <Item
+    number={26}
+    status="open"
+    title="Add a option to expose daemon's TCP port"
+    branch="cumet04:binding_option"
+    iconUrl="https://github.com/identicons/cumet04.png"
+    reviews={[
+      {
+        status: "pending",
+        username: "sveltejs",
+        iconUrl: "https://github.com/identicons/sveltejs.png",
+      },
+      {
+        status: "approve",
+        username: "vitejs",
+        iconUrl: "https://github.com/identicons/vitejs.png",
+      },
+      {
+        status: "comment",
+        username: "cumet04",
+        iconUrl: "https://github.com/identicons/storybookjs.png",
+      },
+      {
+        status: "request-change",
+        username: "cumet04",
+        iconUrl: "https://github.com/identicons/cumet04.png",
+      },
+    ]}
+  />
 </main>
 
 <style>
