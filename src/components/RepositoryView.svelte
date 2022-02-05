814f47ae93ce06041838b08c1f1a6e058aa747a8
cumet04 <medalhkr@gmail.com>
2022-02-05 14:21:59 +0900

    tmp

---
+++ src/components/RepositoryView.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Item from "@/components/Item.svelte";
+
+  export let repository: Repository;
+  const { org, name, pullRequests } = repository;
+
+  const title = `${org}/${name}`;
+</script>
+
+<div />
+
+<style lang="postcss">
+</style>
