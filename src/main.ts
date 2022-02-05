d6916bff11649d3aae1149d23ebcc95a7a702505
cumet04 <medalhkr@gmail.com>
2022-02-05 14:29:14 +0900

    remove all

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "@/main.css";
-import App from "./App.svelte";
-
-const app = new App({
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  target: document.getElementById("app")!,
-});
-
-export default app;
