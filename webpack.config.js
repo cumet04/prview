4bfd003ac446e8fd2f436b3060ac826a9b82523b
cumet04 <medalhkr@gmail.com>
2022-02-06 23:49:02 +0900

    webpack build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", // TODO:
+  entry: "./src/main.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+};
